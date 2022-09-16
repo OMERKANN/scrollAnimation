757b0ab68ae3a327b1c5f355fc77662df4bf8086
Omer <74150804+OMERKANN@users.noreply.github.com>
2022-09-16 23:06:20 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBotton = (window.innerHeight / 5 * 4)
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if(boxTop < triggerBotton ) {
+            box.classList.add("show");
+        }else{
+            box.classList.remove("show");
+        }
+    })
+    
+}
